6a430af23a7a405419bb320d37f8b88af06ed4b2
Robert DeLuca <robertdeluca19@gmail.com>
2022-01-24 11:32:06 -0600

    v1.28.0

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,9 +1,9 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "1.24.0"
+  version "1.28.0"
   url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
-  sha256 "7006685f0bb23c1dd519da139ea341c34bc294e28924d15302112f93823cf5bb"
+  sha256 "3c2577cac9c9edc8d7210c3588fbc484b45a0f04"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
